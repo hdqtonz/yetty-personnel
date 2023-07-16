9313def260f1218e3276f223733f9f32b6eeb5fc
Arpit Santoki <santokiarpit0@gmail.com>
2023-07-16 19:59:04 +0530

    call_table_list_service

--- src/app/modules/tables/tables.component.ts
+++ src/app/modules/tables/tables.component.ts
@@ -3,6 +3,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { BaseComponent } from 'src/app/core/class/base-component';
 import { AccountService } from 'src/app/core/services/account.service';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { Establishment } from '../../core/interface/Establishment';
+import { EstablishmentSettings } from '../../core/interface/EstablishmentSettings';
+import { Table } from '../../core/interface/Table';
+import { TableService } from '../../core/services/table.service';
 
 @Component({
   selector: 'app-tables',
@@ -10,9 +14,14 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./tables.component.scss'],
 })
 export class TablesComponent extends BaseComponent implements OnInit {
+
+  // Data variable
+  public TableList: Table;
+
   constructor(
     private _matSnackkBar: MatSnackBar,
-    private _accountService: AccountService
+    private _accountService: AccountService,
+    private _tableService: TableService
   ) {
     super(_matSnackkBar);
 
@@ -20,7 +29,73 @@ export class TablesComponent extends BaseComponent implements OnInit {
     console.log(user);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getEstablishmentsTableList();
+  }
 
   selectedTable: string = 'my';
+
+  /**
+   * Get Establishments Settings
+   */
+  getEstablishmentsSettings() {
+    this.isLoading = true;
+    this._tableService.getEstablishmentSettings()
+      .subscribe({
+        next: (res: EstablishmentSettings) => {
+          this.isLoading = false;
+          console.log(res);
+        },
+        error: (err) => {
+          this.isLoading = false;
+          this.showError(err?.message);
+        },
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
+  }
+
+  /**
+   * Get Establishments Info
+   */
+  getEstablishmentsInfo() {
+    this.isLoading = true;
+    this._tableService.getEstablishmentInfo()
+      .subscribe({
+        next: (res: Establishment) => {
+          this.isLoading = false;
+          console.log(res);
+        },
+        error: (err) => {
+          this.isLoading = false;
+          this.showError(err?.message);
+        },
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
+  }
+
+  /**
+   * Get Establishments Table List
+   */
+  getEstablishmentsTableList() {
+    this.isLoading = true;
+    this._tableService.listTables(true)
+      .subscribe({
+        next: (res: Table) => {
+          this.TableList = res;
+          this.isLoading = false;
+          console.log(this.TableList);
+        },
+        error: (err) => {
+          this.isLoading = false;
+          this.showError(err?.message);
+        },
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
+  }
 }
